3fb9180b8fa9dca3dd008ce9f951a975de4a845b
Pablo Anttila <fimbo@Pablos-MacBook-Pro.local>
2018-09-11 17:21:16 +0200

    fix: rename main container to comply with wireframe

--- src/containers/Explore.js
+++ src/containers/Explore.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from '../static/logo.svg';
-import './App.css';
+import { default as NavBar } from '../components/navbar/NavBar'
+
+import './Explore.css';
 
 class App extends Component {
   constructor(){
@@ -26,10 +27,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <NavBar
+          title="Look into space"
+          />
         {this.props.children}
         <p className="App-intro">
           {this.state.mounted.id || 'Welcome to Nasa. Choose one'}
--- src/containers/Explore.test.js
+++ src/containers/Explore.test.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Explore from './explore';
 
 import { shallow } from 'enzyme';
 
 describe('App Container', () => {
   it('Renders', () => {
-    const wrapper = shallow(<App />);
-    const printOut = <h1 className="App-title">Welcome to React</h1>;
+    const wrapper = shallow(<Explore />);
+    const printOut = <p className="App-intro">Welcome to Nasa. Choose one</p>;
     expect(wrapper.contains(printOut)).toEqual(true);
   });
   it('Renders children when passed in', () => {
-    const wrapper = shallow(<App><div className="block_element--modifier" /></App>);
+    const wrapper = shallow(<Explore><div className="block_element--modifier" /></Explore>);
     expect(wrapper.contains(<div className="block_element--modifier" />)).toEqual(true);
   });
   it('Has an initial state', () =>{
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<Explore />);
     expect(wrapper.state('explore')).toEqual(true);
     expect(wrapper.state('mounted')).toEqual({'id':null, 'state':null});
   });
   it('Changes state on user actions', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<Explore />);
     expect(wrapper.find('.rover').length).toEqual(3);
 
     wrapper.find('.rover.curiosity a').simulate('click');
