2c16f0bd82bddc708a26a36fb8802ac9f39ca10e
Pablo Anttila <fimbo@Pablos-MacBook-Pro.local>
2018-09-11 18:53:53 +0200

    Feat: Add navbar as component

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -4,10 +4,27 @@ import logo from '../../static/logo.svg';
 export default (props) => {
   return (
     <header className="header">
-      <nav>
-        <img src={logo} className="App-logo" alt="logo" />
-        <a className="brand">{props.title}</a>
-        <a className="apod-widget">Photo of The day</a>
+      <nav className="navbar is-transparent">
+        <div className="navbar-brand">
+          <a className="navbar-item">
+            <img src={logo} className="App-logo" alt="app-logo" width="50" height="50" />
+            {props.title}
+          </a>
+          <div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <div id="navbarExampleTransparentExample" className="navbar-menu">
+
+          <div className="navbar-end">
+            <a className="navbar-item">
+              Nasa Photo of the day
+            </a>
+          </div>
+        </div>
       </nav>
     </header>
   )
